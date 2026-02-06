98fad4d1d383003a20e74f3183c34cf3269fd3b7
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-06 18:45:11 +0100

    Deploy Astro site Cloudflare Pages 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
