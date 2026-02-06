2e7477f295e1c980a220a46ea79c52d319bb4168
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-06 18:29:20 +0100

    Deploy Astro site Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// filepath: /Users/normanleyvagarcia/Desktop/Astro/barcelonatango/astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
