f68891b84610bb69639a290ffa8cde11a479262a
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-06 19:27:10 +0100

    Force rebuild - verify server output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
+// Cloudflare Workers deployment
 export default defineConfig({
   integrations: [tailwind()],
   adapter: cloudflare(),
