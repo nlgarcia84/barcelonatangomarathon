d4e13eead6d7590bd375e6c963d54cb3f1598702
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-06 19:29:24 +0100

    Switch to static output for proper CSS serving

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [tailwind()],
   adapter: cloudflare(),
-  output: 'server',
+  output: 'static',
   image: {
     service: {
       entrypoint: 'astro/assets/services/default',
