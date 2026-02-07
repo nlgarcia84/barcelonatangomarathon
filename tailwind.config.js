66efba21deb517ff065d20bacf27e75a408bd391
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-07 17:58:36 +0100

    added extensions for font families in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        inder: ['Inder', 'sans-serif'],
+        noto: ['Noto Sans', 'sans-serif'],
+        sn: ['SN Pro', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
