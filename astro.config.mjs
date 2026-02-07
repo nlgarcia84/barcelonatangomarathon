312af71d7efba831489a223f8d21a608f39d85e1
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-07 16:29:54 +0100

    astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
-// Cloudflare Workers deployment
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: cloudflare(),
-  output: 'server',
+  output: 'static',
   image: {
     service: {
-      entrypoint: 'astro/assets/services/default',
-    },
-  },
-  vite: {
-    ssr: {
-      external: ['node:async_hooks'],
+      entrypoint: 'astro/assets/services/compile',
     },
   },
 });
